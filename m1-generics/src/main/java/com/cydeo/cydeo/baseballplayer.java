0b85845825ce5f3752262666710c9eee6660ce9d
emrulla muhter <emrullauigur@gmai.com>
2023-07-06 19:42:44 +0300

    create classes

---
+++ m1-generics/src/main/java/com/cydeo/cydeo/baseballplayer.java
@@ -0,0 +1,7 @@
+package com.cydeo.cydeo;
+
+public class baseballplayer extends player{
+    public baseballplayer(String name) {
+        super(name);
+    }
+}
---
+++ m1-generics/src/main/java/com/cydeo/cydeo/player.java
@@ -0,0 +1,12 @@
+package com.cydeo.cydeo;
+
+public abstract class player {
+   private String name;
+   public player(String name){
+       this.name=name;
+   }
+
+    public String getName() {
+        return name;
+    }
+}
