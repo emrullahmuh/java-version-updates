9163c02129101f25abd247cdf6f83fc4c92c6979
emrulla muhter <emrullauigur@gmai.com>
2023-07-07 02:16:03 +0300

    try to learn something new

---
+++ m2-enum_2/src/main/java/com/cydeo/Operations.java
@@ -0,0 +1,5 @@
+package com.cydeo;
+
+public enum Operations {
+    ATRI,EKSI,CARPI,BOLME
+}
---
+++ m2-enum_2/src/main/java/com/cydeo/currencyDemo.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class currencyDemo {
+
+    public static final currencyDemo penny=new currencyDemo();
+    public static final currencyDemo adem=new currencyDemo();
+    public static final currencyDemo odin= new currencyDemo();
+    public static final currencyDemo vandal =new currencyDemo();
+}
+
